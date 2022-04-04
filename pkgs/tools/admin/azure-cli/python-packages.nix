14ab642a092d04bd66933bdbc268b459c0492c53
Peter Romfeld <peter.romfeld.hk@gmail.com>
2022-04-04 14:50:33 +0300

    asd

--- pkgs/tools/admin/azure-cli/python-packages.nix
+++ pkgs/tools/admin/azure-cli/python-packages.nix
@@ -398,12 +398,18 @@ let
           sha256 = "0jfxm8lx8dzs3v2b04ljizk8gfckbm5l2v86rm7k0npbfvryba1p";
         };
 
+        preBuild = ''
+          rm -f azure_bdist_wheel.py
+          substituteInPlace setup.cfg \
+            --replace "azure-namespace-package = azure-mgmt-nspkg" ""
+        '';
+
         propagatedBuildInputs = with self; [
-          azure-common
+          # azure-common azure-nspkg
           super.azure-keyvault-certificates super.azure-keyvault-secrets azure-keyvault-keys
-          msrest msrestazure cryptography
+          # msrest msrestazure cryptography
         ];
-        pythonNamespaces = [ "azure" "azure.keyvault" ];
+        pythonNamespaces = [ "azure.keyvault" ];
         pythonImportsCheck = [
           "azure.keyvault"
           "azure.keyvault.keys"
